5762cac0dd3006af773e03b1f4943b8593df8cda
YeshuaChiliquingaAmaya <yeshuachiliquinga@gmail.com>
2024-12-09 07:25:28 -0500

    update files

--- Mahanaym_System/src/App.jsx
+++ Mahanaym_System/src/App.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
-import "bootstrap/dist/css/bootstrap.min.css";
 import Login from "./Pages/Login";
 import Home from "./Pages/Home";
 import Navbar from "./Components/Navbar";
